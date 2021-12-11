34e0256a7dabd44dde9e9f14962c4df83b937b7c
Andrea Mininni <mininniandrea@gmail.com>
2021-12-11 19:19:18 +0100

    views setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-
+const app = express();
 const mongoConnect = require('./utils/database').mongoConnect;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 mongoConnect( ( ) => {
     app.listen(3000);
